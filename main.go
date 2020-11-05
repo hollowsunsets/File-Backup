e7ad004a294d4235e01ade4c321a5bc72578cadc
Isabel Giang <isabelgiang@gmail.com>
2020-11-04 22:12:00 -0800

    Add basic CLI skeleton

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide an action for the tool to take")
+		os.Exit(1)
+	}
+	switch os.Args[1] {
+	case "backup":
+		fmt.Println("backup")
+	case "restore":
+		fmt.Println("restore")
+	default:
+		fmt.Printf("Unsupported action: %s\n", os.Args[1])
+		os.Exit(1)
+	}
+
+	bucketName := flag.String("bucketName", "", "Name of S3 bucket")
+	directoryName := flag.String("directoryName", "", "Name of file directory")
+	flag.Parse()
+
+	if *bucketName == "" || *directoryName == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+}
+
+func backup(directoryName string, bucketName string) {
+
+}
+
+func restore(bucketName string, directoryName string) {
+
 }
