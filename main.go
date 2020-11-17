ef3bf07d8bdc46a806899b6e46b0f3caf54fa591
Isabel Giang <isabelgiang@gmail.com>
2020-11-17 00:13:23 -0800

    Remove hardcoded AWS region

--- main.go
+++ main.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/hollowsunsets/fcopy/cmd"
 	"os"
 )
 
 func main() {
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-west-2"),
-	})
+	sess, err := session.NewSession()
 	_, err = sess.Config.Credentials.Get()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
